1b8a352b4113fb61fff8b17130c7aacc85c433b4
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-06-29 18:53:32 +0200

    Adding-View: added a simple for displaying the page content

---
+++ view/pageTemplate.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo htmlspecialchars($title); ?></title>
+</head>
+<body>
+<h1><?php echo htmlspecialchars($title); ?></h1>
+<p><?php echo htmlspecialchars($description); ?></p>
+</body>
+</html>
