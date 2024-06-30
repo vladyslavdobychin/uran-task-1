da4ab43c5f9637dd77c42e45f08548af2d01944e
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-06-30 09:10:00 +0200

    Entry-Point: refactoring request handler

--- functions.php
+++ functions.php
@@ -6,23 +6,34 @@ function redirect($url) {
 }
 
 function handleRequest($controller) {
-    if (isset($_GET['id'])) {
-        $identifier = $_GET['id'];
-        $controller->displayPage($identifier);
-    } elseif (isset($_GET['friendly'])) {
-        $identifier = $_GET['friendly'];
-        if ($identifier === 'home') {
-            $controller->displayHomePage();
-        } else {
+    $requestUri = $_SERVER['REQUEST_URI'];
+    $normalizedUri = strtok($requestUri, '?');
+
+    switch (true) {
+        case isset($_GET['id']):
+            $identifier = $_GET['id'];
             $controller->displayPage($identifier);
-        }
-    } elseif ($_SERVER['REQUEST_URI'] === '/') {
-        // Redirect to the friendly URL of the home page
-        redirect('/home');
-    } elseif ($_SERVER['REQUEST_URI'] === '/home') {
-        $controller->displayHomePage();
-    } else {
-        // Handle invalid routes or unknown pages
-        redirect('/home'); // Default to home page
+            break;
+
+        case isset($_GET['friendly']):
+            $identifier = $_GET['friendly'];
+            if ($identifier === 'home') {
+                $controller->displayHomePage();
+            } else {
+                $controller->displayPage($identifier);
+            }
+            break;
+
+        case $normalizedUri === '/':
+            redirect('/home');
+            break;
+
+        case $normalizedUri === '/home':
+            $controller->displayHomePage();
+            break;
+
+        default:
+            redirect('/home'); // Default to home page
+            break;
     }
 }
